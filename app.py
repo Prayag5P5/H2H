a777523a7fee79bcdf8e6f5b077508972ff1ccc9
Prayag5P5 <91774576+Prayag5P5@users.noreply.github.com>
2021-12-19 13:33:02 +0530

    Add files via upload
    New files

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 
 app = Flask(__name__)
-app.secret_key = "secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:////mnt/c/Users/<username>/Documents/login_example/login.db'
+app.config['SECRET_KEY'] = 'secret_key'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route("/")
 def hello_world():
@@ -24,6 +41,25 @@ def user():
 
     else:
         return redirect(url_for("login"))
+
+# @app.route('/')
+# def index():
+#     user = User.query.filter_by(username='Prayag').first()
+#     login_user(user)
+#     return 'You are now logged in!'
+
+
+# @app.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     return 'You are now logged out!'
+
+
+# @app.route('/home')
+# @login_required
+# def home():
+#     return 'The current user is ' + current_user.username
 
 if __name__ == "main":
     app.run(debug=True)
